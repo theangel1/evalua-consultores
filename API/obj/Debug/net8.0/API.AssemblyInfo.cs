cf98ad8b7d3d831baf0cbca138cc8cc4d16353a1
theAngel1 <angel.pinilla@outlook.es>
2025-09-14 19:44:36 -0300

    final push

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd795ae5fd8ce54d68d9c7e5d014472f8c3e2a1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef7aca5898f283319daa2e3107f85128ed2aee19")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
